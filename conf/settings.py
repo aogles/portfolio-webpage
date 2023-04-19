1657acaed777cbbaa7c0587f0e996346ab18fbec
Amber Oglesby <aoglesby5795@gmail.com>
2023-04-19 00:19:07 -0400

    add react-app-dir to settings.py to fixed heroku deployed site

--- conf/settings.py
+++ conf/settings.py
@@ -162,6 +162,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend/static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "frontend/static/build/static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
