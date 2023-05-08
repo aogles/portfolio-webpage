55d1b076b16e7aee27612db4947027cba5100882
Amber Oglesby <aoglesby5795@gmail.com>
2023-05-07 23:41:47 -0400

    update contact model, begin set up for fetch request, add contact submission form

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -1,8 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [contactMessage, setContactMessage] = useState("");
+
+  useEffect(() => {
+    const getContactInfo = async () => {
+      const response = await fetch("/api_v1/blogs/");
+      if (!response.ok) {
+        throw new Error("Network response was not OK");
+      }
+
+      const data = await response.json();
+      setContacts(data);
+    };
+
+    getContacts();
+  }, []);
+
   return (
     <div className="App">
       <nav>
@@ -10,6 +33,34 @@ function App() {
         <Header />
       </nav>
       <h1 id="page-title">Hello World,Im Amber</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="text" placeholder="name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Phone</Form.Label>
+          <Form.Control type="text" placeholder="name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>message</Form.Label>
+          <Form.Control type="text" placeholder="name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
