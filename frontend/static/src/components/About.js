f8b360c138c138e70ee265bd00b9e739c71eb1a6
Amber Oglesby <aoglesby5795@gmail.com>
2023-06-04 12:10:21 -0400

    add styling to about me page and animations

--- frontend/static/src/components/About.js
+++ frontend/static/src/components/About.js
@@ -1,7 +1,5 @@
 import Header from "./Header";
 import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Fade from "react-bootstrap/Fade";
 
 function About() {
   const [open, setOpen] = useState(false);
@@ -9,38 +7,25 @@ function About() {
   return (
     <div>
       <Header />
-      <div className="aboutMe">
-        <>
-          <Button
-            onClick={() => setOpen(!open)}
-            aria-controls="example-fade-text"
-            aria-expanded={open}
-          >
-            About Me
-          </Button>
-          <Fade in={open}>
-            <div id="example-fade-text">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life
-              accusamus terry richardson ad squid. Nihil anim keffiyeh
-              helvetica, craft beer labore wes anderson cred nesciunt sapiente
-              ea proident. Anim pariatur cliche reprehenderit, enim eiusmod high
-              life accusamus terry richardson ad squid. Nihil anim keffiyeh
-              helvetica, craft beer labore wes anderson cred nesciunt sapiente
-              ea proident. Anim pariatur cliche reprehenderit, enim eiusmod high
-              life accusamus terry richardson ad squid. Nihil anim keffiyeh
-              helvetica, craft beer labore wes anderson cred nesciunt sapiente
-              ea proident. Anim pariatur cliche reprehenderit, enim eiusmod high
-              life accusamus terry richardson ad squid. Nihil anim keffiyeh
-              helvetica, craft beer labore wes anderson cred nesciunt sapiente
-              ea proident. Anim pariatur cliche reprehenderit, enim eiusmod high
-              life accusamus terry richardson ad squid. Nihil anim keffiyeh
-              helvetica, craft beer labore wes anderson cred nesciunt sapiente
-              ea proident.
-            </div>
-          </Fade>
-        </>
-      </div>
-      <div className="fadein-text">Testing Animation</div>
+      <article className="aboutMe">
+        <div className="fadein-text">
+          Testing Animation Anim pariatur cliche reprehenderit, enim eiusmod
+          high life accusamus terry richardson ad squid. Nihil anim keffiyeh
+          helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+          proident. Anim pariatur cliche reprehenderit, enim eiusmod high life
+          accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica,
+          craft beer labore wes anderson cred nesciunt sapiente ea proident.
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+          labore wes anderson cred nesciunt sapiente ea proident. Anim pariatur
+          cliche reprehenderit, enim eiusmod high life accusamus terry
+          richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore
+          wes anderson cred nesciunt sapiente ea proident. Anim pariatur cliche
+          reprehenderit, enim eiusmod high life accusamus terry richardson ad
+          squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
+          cred nesciunt sapiente ea proident.
+        </div>
+      </article>
     </div>
   );
 }
--- frontend/static/src/components/Work.js
+++ frontend/static/src/components/Work.js
@@ -1,10 +1,43 @@
 import Header from "./Header";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Fade from "react-bootstrap/Fade";
 
 function Work() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
       <Header />
       <h1>Work Page</h1>;
+      <>
+        <Button
+          onClick={() => setOpen(!open)}
+          aria-controls="example-fade-text"
+          aria-expanded={open}
+        >
+          About Me
+        </Button>
+        <Fade in={open}>
+          <div id="example-fade-text">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+            labore wes anderson cred nesciunt sapiente ea proident. Anim
+            pariatur cliche reprehenderit, enim eiusmod high life accusamus
+            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+            labore wes anderson cred nesciunt sapiente ea proident. Anim
+            pariatur cliche reprehenderit, enim eiusmod high life accusamus
+            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+            labore wes anderson cred nesciunt sapiente ea proident. Anim
+            pariatur cliche reprehenderit, enim eiusmod high life accusamus
+            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+            labore wes anderson cred nesciunt sapiente ea proident. Anim
+            pariatur cliche reprehenderit, enim eiusmod high life accusamus
+            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+            labore wes anderson cred nesciunt sapiente ea proident.
+          </div>
+        </Fade>
+      </>
     </div>
   );
 }
