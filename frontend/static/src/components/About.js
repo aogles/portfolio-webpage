354e4d07ffdf18d345560792cbef13f6133d3f44
Amber Oglesby <aoglesby5795@gmail.com>
2023-06-22 14:47:17 -0400

    organize about me paragraphs

--- frontend/static/src/components/About.js
+++ frontend/static/src/components/About.js
@@ -17,21 +17,37 @@ function About() {
 
       <article className="aboutMe">
         <div className="fadein-text">
+          <p className="medical">
+            <img src="https://cdn0.iconfinder.com/data/icons/education-color/128/education_color-14-2-256.png"></img>
+          </p>
           <h1>About me professionally </h1>
-          As a highly motivated and disciplined individual with a passion for
-          problem-solving, I am excited to apply my skills and experience to the
-          field of software development. After six years in direct patient care
-          roles, I have honed my ability to adapt quickly to new challenges,
-          work collaboratively with diverse teams, and communicate effectively
-          under pressure. Throughout my career in healthcare, I gained valuable
-          experience in managing complex workflows, analyzing data, and
-          leveraging technology to improve patient outcomes. As an Army officer,
-          I developed skills in leadership, strategic planning, and project
-          management that are highly applicable to software development. I am
-          eager to leverage these skills to build innovative solutions that meet
-          the needs of users and businesses alike. Whether it's developing new
-          applications or improving existing ones, I am committed to delivering
-          high-quality, user-focused products that make a positive impact.
+          <p>
+            As a highly motivated and disciplined individual with a passion for
+            problem-solving, I am excited to apply my skills and experience to
+            the field of software development.
+          </p>
+          <p>
+            {" "}
+            After six years in direct patient care roles, I have honed my
+            ability to adapt quickly to new challenges, work collaboratively
+            with diverse teams, and communicate effectively under pressure.
+            Throughout my career in healthcare, I gained valuable experience in
+            managing complex workflows, analyzing data, and leveraging
+            technology to improve patient outcomes.
+          </p>
+          <p>
+            As an Army officer, I developed skills in leadership, strategic
+            planning, and project management that are highly applicable to
+            software development. I am eager to leverage these skills to build
+            innovative solutions that meet the needs of users and businesses
+            alike. Whether it's developing new applications or improving
+            existing ones, I am committed to delivering high-quality,
+            user-focused products that make a positive impact.
+          </p>
+
+          <p className="medal">
+            <img src="https://cdn4.iconfinder.com/data/icons/military-and-army-2/64/Army_1-05-256.png"></img>
+          </p>
         </div>
       </article>
 
