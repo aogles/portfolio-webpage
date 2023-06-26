29aad19c41442c6e23ea8f6802088d3ba3ea1f3a
Amber Oglesby <aoglesby5795@gmail.com>
2023-06-25 21:02:03 -0400

    add experience logos and links

--- frontend/static/src/components/Work.js
+++ frontend/static/src/components/Work.js
@@ -3,7 +3,13 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Fade from "react-bootstrap/Fade";
 import { FaReact, FaGithub, FaPython, FaBootstrap } from "react-icons/fa";
-import { BiLogoDjango, BiLogoJavascript } from "react-icons/bi";
+import { FaTruckFront } from "react-icons/fa6";
+import { BiLogoDjango, BiLogoJavascript, BiNavigation } from "react-icons/bi";
+import { CgCopy } from "react-icons/cg";
+import { BsFillFileWordFill } from "react-icons/bs";
+import { GiGamepadCross } from "react-icons/gi";
+import { convoy } from "../Images/convoy-commander.png";
+import { game } from "../Images/game-logo.png";
 
 function Work() {
   const [open, setOpen] = useState(false);
@@ -37,16 +43,26 @@ function Work() {
             </div>
             <h2>Experience</h2>
             <div>
-              <div class="job-title">
-                <div className="Company">
-                  <h6 className="job-title2">Front End Engineer"</h6>
+              <div className="job-title">
+                <div className="Company ">
+                  <a>
+                    <img
+                      id="convoy"
+                      src={require("../Images/convoy-commander.png")}
+                    ></img>
+                  </a>
+                  Convoy Trainer
+                  <h6 className="job-title2">Full Stack Engineer</h6>
                   <h6 className="stack-logo">
-                    <a href="#" target="__blank">
-                      stack used
+                    <a
+                      href="https://convoy-commander.herokuapp.com/"
+                      target="__blank"
+                    >
+                      Convoy Conmmander
                     </a>
                   </h6>
                 </div>
-                <div class="timeframe">Oct 2022 - Present</div>
+                <div className="timeframe">January 2023 - Present</div>
               </div>
               <div class="job-details">
                 <p>Helping build better API docs</p>
@@ -62,15 +78,22 @@ function Work() {
 
             <div>
               <div class="job-title">
-                <div className="Company">
-                  <h6 className="job-title2">Front End Engineer"</h6>
+                <div className="Company ccs">
+                  <a>
+                    <img src="https://avatars.githubusercontent.com/u/121811161?s=64&v=4"></img>
+                  </a>
+                  Carolina Code School
+                  <h6 className="job-title2">Front End Engineer</h6>
                   <h6 className="stack-logo">
-                    <a href="#" target="__blank">
-                      stack used
+                    <a
+                      href="https://aogles.github.io/pixel-perfect"
+                      target="__blank"
+                    >
+                      Pixel Perfect
                     </a>
                   </h6>
                 </div>
-                <div class="timeframe">Oct 2022 - Present</div>
+                <div class="timeframe">January 2023 - February 2023</div>
               </div>
               <div class="job-details">
                 <p>Helping build better API docs</p>
@@ -86,14 +109,21 @@ function Work() {
             <div>
               <div class="job-title">
                 <div className="Company">
-                  <h6 className="job-title2">Front End Engineer"</h6>
+                  <p>
+                    <BsFillFileWordFill />
+                  </p>
+                  <div className="wordle">Wordle</div>
+                  <h6 className="job-title2">Front End Engineer</h6>
                   <h6 className="stack-logo">
-                    <a href="#" target="__blank">
-                      stack used
+                    <a
+                      href="replit.com/@aogles/pythonwordlegame"
+                      target="__blank"
+                    >
+                      Python Wordle
                     </a>
                   </h6>
                 </div>
-                <div class="timeframe">Oct 2022 - Present</div>
+                <div class="timeframe">March 2023</div>
               </div>
               <div class="job-details">
                 <p>Helping build better API docs</p>
@@ -109,37 +139,24 @@ function Work() {
             <div>
               <div class="job-title">
                 <div className="Company">
-                  <h6 className="job-title2">Front End Engineer"</h6>
+                  <a>
+                    <img
+                      id="game"
+                      src={require("../Images/game-logo.png")}
+                    ></img>
+                  </a>
+                  Amateur Games
+                  <h6 className="job-title2">Front End Engineer</h6>
                   <h6 className="stack-logo">
-                    <a href="#" target="__blank">
-                      stack used
+                    <a
+                      href="https://aogles.github.io/react_connect_4_app/"
+                      target="__blank"
+                    >
+                      Connect 4
                     </a>
                   </h6>
                 </div>
-                <div class="timeframe">Oct 2022 - Present</div>
-              </div>
-              <div class="job-details">
-                <p>Helping build better API docs</p>
-                <ul>
-                  <li>
-                    Building out new Metrics features for data-driven API
-                    insights
-                  </li>
-                </ul>
-                <hr></hr>
-              </div>
-            </div>
-            <div>
-              <div class="job-title">
-                <div className="Company">
-                  <h6 className="job-title2">Front End Engineer"</h6>
-                  <h6 className="stack-logo">
-                    <a href="#" target="__blank">
-                      stack used
-                    </a>
-                  </h6>
-                </div>
-                <div class="timeframe">Oct 2022 - Present</div>
+                <div class="timeframe">May 2023</div>
               </div>
               <div class="job-details">
                 <p>Helping build better API docs</p>
